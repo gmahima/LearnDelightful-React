95118469a00e7189b78f23117d6b803090b86934
Mahima Gangavarapu <39587007+gmahima@users.noreply.github.com>
2019-12-08 23:58:13 +0530

    add Job component

--- src/index.js
+++ src/index.js
@@ -4,19 +4,27 @@ import { func } from "prop-types";
 
 const mountNode = document.getElementById("root");
 
+function Job(props) {
+  return (
+    <li>
+      {
+        <article className="media job">
+          <div className="media-content">
+            <h4>{props.title}</h4>
+          </div>
+        </article>
+      }
+    </li>
+  );
+}
+
 function FilterJobs() {
   return null;
 }
 
 function JobsList() {
   const jobTitles = ["Lead Engineer", "Junior Engineer", "some other Engineer"];
-  const elements = jobTitles.map(title => (
-    <article className="media job">
-      <div className="media-content">
-        <h4>{title}</h4>
-      </div>
-    </article>
-  ));
+  const elements = jobTitles.map(title => <Job title={title} />);
   return <ol>{elements}</ol>;
 }
 
