ff9a864cea7983e661701f7c1d44af1896a2b123
Mahima Gangavarapu <39587007+gmahima@users.noreply.github.com>
2019-12-08 23:50:48 +0530

    map over array

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { func } from "prop-types";
 
 const mountNode = document.getElementById("root");
+
+function FilterJobs() {
+  return null;
+}
+
+function JobsList() {
+  const jobTitles = ["Lead Engineer", "Junior Engineer", "some other Engineer"];
+  const elements = jobTitles.map(title => (
+    <article className="media job">
+      <div className="media-content">
+        <h4>{title}</h4>
+      </div>
+    </article>
+  ));
+  return <ol>{elements}</ol>;
+}
+
 function App() {
   return (
     <div>
-      <p>hello</p>
-      <span>{1}</span>
+      <FilterJobs />
+      <JobsList />
     </div>
   );
 }
+
 ReactDOM.render(<App />, mountNode);
