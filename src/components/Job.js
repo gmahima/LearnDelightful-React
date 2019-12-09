5c72740e83bede5e72ab60c5d22d4197c3317548
Mahima Gangavarapu <39587007+gmahima@users.noreply.github.com>
2019-12-09 19:40:28 +0530

    add useState hook

--- src/components/Job.js
+++ src/components/Job.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+
 export default function Job(props) {
+  const [isDetail, setIsDetail] = useState(false);
   const { title, desc } = props.job;
+  function handleClick() {
+    setIsDetail(true);
+  }
   return (
     <li>
       {
-        <article className="media job">
+        <article onClick={handleClick} className="media job">
           <div className="media-content">
             <h4>{title}</h4>
-            <p>{desc}</p>
+            {isDetail ? <p>{desc}</p> : null}
           </div>
         </article>
       }
