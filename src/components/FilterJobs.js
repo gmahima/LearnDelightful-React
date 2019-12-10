df3f0c1ceac4911761f77e709260ea575eb86d5b
Mahima Gangavarapu <39587007+gmahima@users.noreply.github.com>
2019-12-10 14:52:46 +0530

    add checkboxes

--- src/components/FilterJobs.js
+++ src/components/FilterJobs.js
@@ -11,12 +11,57 @@ export default function FilterJobs({
     const newVal = event.target.value;
     setSearchText(newVal);
   }
+  function handleShowOnlyFeatured(event) {
+    setShowOnlyFeatured(event.target.checked);
+  }
+  function handleShowOnlyRemote(event) {
+    setShowOnlyRemote(event.target.checked);
+  }
   return (
     <section className="section filter-jobs">
       <h1>Search Jobs</h1>
-      <div className="field has-addons">
-        <div className="control is-expanded">
-          <input className="input" value={searchText} onChange={handleChange} />
+      <div>
+        <div className="field has-addons">
+          <div className="control is-expanded">
+            <input
+              className="input"
+              value={searchText}
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+        <div className="field">
+          <label className="label">Options</label>
+        </div>
+        <div className="field is-grouped">
+          <div className="control">
+            <label className="checkbox" htmlFor="featured">
+              <input
+                id="featured"
+                type="checkbox"
+                checked={showOnlyFeatured}
+                onChange={event => {
+                  console.log(event.target.checked);
+                  return handleShowOnlyFeatured(event);
+                }}
+              />
+              Featured
+            </label>
+          </div>
+          <div className="control">
+            <label className="checkbox" htmlFor="remote">
+              <input
+                id="remote"
+                type="checkbox"
+                checked={showOnlyRemote}
+                onChange={event => {
+                  console.log(event.target.checked);
+                  return handleShowOnlyRemote(event);
+                }}
+              />
+              Remote
+            </label>
+          </div>
         </div>
       </div>
     </section>
--- src/components/JobList.js
+++ src/components/JobList.js
@@ -3,9 +3,16 @@ import Job from "./Job";
 import jobs from "../data/jobData";
 export default function JobsList(props) {
   const { searchText, showOnlyFeatured, showOnlyRemote } = props;
-  const filteredJobs = jobs.filter(job => {
+  let filteredJobs = jobs.filter(job => {
     return job.title.toLowerCase().includes(searchText.toLowerCase());
   });
+  if (showOnlyFeatured) {
+    filteredJobs = filteredJobs.filter(job => job.isFeatured);
+  }
+
+  if (showOnlyRemote) {
+    filteredJobs = filteredJobs.filter(job => job.isRemote);
+  }
   return (
     <div>
       <ol>
